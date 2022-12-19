e478d1f919d3bb4b719b44b6459822faa3379714
CandiceBrasileiro <candicelopes88@gmail.com>
2022-12-19 11:07:33 -0300

    criação de partials, usando bootstrap e envio de form

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-// Usar o ejs como view engine
+// Usar o ejs como tamplate engine
 app.set('view engine', 'ejs');
 // Carregar arquivos estáticos
 app.use(express.static('public'));
 
+//body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//rotas
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+})
+
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send("Formulário recebido! titulo" + titulo + "descricao" + descricao);
 })
 
 app.listen(8080,()=>{console.log("App rodando!")});
